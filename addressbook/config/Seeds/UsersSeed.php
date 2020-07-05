02eed888f33f1a6cc1004fe58bed5b6502df657f
Shinsuke Obata <studio15.jp@gmail.com>
2020-07-05 16:47:33 +0900

    Add UsersSeed

---
+++ addressbook/config/Seeds/UsersSeed.php
@@ -0,0 +1,36 @@
+<?php
+use Migrations\AbstractSeed;
+
+/**
+ * Users seed.
+ */
+class UsersSeed extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeds is available here:
+     * https://book.cakephp.org/phinx/0/en/seeding.html
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'name' => 'Shinsuke Obata',
+                'sex' => 1,
+                'sex_description' => null,
+                'phone' => '+819061496245',
+                'email' => 'info@studio15.jp',
+                'created' => date('Y-m-d H:i:s'),
+                'modified' => date('Y-m-d H:i:s'),
+            ],
+        ];
+
+        $table = $this->table('users');
+        $table->insert($data)->save();
+    }
+}
